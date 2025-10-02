f361a8e3f06197573baae91bbeddbe9fb643d42f
EMA ZAJASENSKA <sadpeople844@gmail.com>
2025-10-02 09:36:46 +0200

    Prepis celého kódu programu

---
+++ Links/micko3.py
@@ -0,0 +1,73 @@
+# uloha 1
+# a= int(input("zadaj a:"))
+# b= int(input("zadaj b:"))
+# if a+b >100:
+#     print("presiahol 100")
+# else:
+#     print("nepresiahol 100")
+
+
+# # uloha 2
+# a=int(input("zadaj cislo:"))
+# print("prevratena hodnota je:",1/a)
+# # na vzpocet treba oddelit ,
+
+# uloha 3
+# a= int(input("zadaj a:"))
+# b= int(input("zadaj b:"))
+# x= int(input("zadaj x:"))
+# if a<=x and x<=b:
+#     print('patri do intervalu')
+# else:
+#     print('nepatri do intervalu')
+
+
+# # uloha 4
+# a= int(input("zadaj cislo:"))
+# if a % 2 == 0:
+#     print("cislo je parne")
+# else:
+#     print("cislo je neparne")
+# # %zvysok po deleni ==
+
+
+# # uloha 5
+# a=int(input('zadaj cislo:'))
+# if a % 3 == 0:
+#     print("cislo je delitelne troma")
+# else:
+#     print("cislo nie je delitelne troma")
+
+
+# # uloha 6
+# a=int(input('zadaj cislo:'))
+# if a % 4 ==0 and a % 7 ==0:
+#     print("cislo je delitelne 4 a siedmymi")
+# elif a % 4 ==0 and a % 7 !=0:
+#     print("cislo je delitelne 4 ale nie siedmymi")
+# elif a % 4 !=0 and a % 7 ==0:
+#     print("cislo je delitelne siedmimy ale nie 4")
+# else:
+#     print("cislo nie je delitelne 4 ani siedmymi")
+# # != nerovna sa
+
+
+# # uloha 7
+# a=int(input("zadaj a:"))
+# b=int(input("zadaj b:"))
+# if a==b:
+#     print("su rovnake")
+# elif a>b:
+#     print (a)
+# else:
+#     print (b)
+
+
+# # uloha 8
+# a=int(input("zadaj cislo:"))
+# if a>0:
+#     print("cislo je kladne")
+# elif a<0:
+#     print("cislo je zaporne")
+# else:
+#     print("cislo je nula")
